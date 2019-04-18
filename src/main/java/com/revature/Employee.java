be750e6e07e2c554717ed6ccebeefe02c35b6b16
yuri0 <yuri0@10.182.4.243>
2019-04-17 21:37:37 -0500

    Testing to add Employee class

---
+++ src/main/java/com/revature/Employee.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.revature;
+
+/**
+ * @author Thanh Vu
+ *
+ */
+public class Employee {
+
+	/**
+	 * 
+	 */
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
