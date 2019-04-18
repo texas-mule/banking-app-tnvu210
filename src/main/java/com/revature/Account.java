8c727d07d171a969a27044cd6a21a70ab0383b08
ThanhVu <yuri0@10.182.4.243>
2019-04-17 23:03:50 -0500

    Add Account class

---
+++ src/main/java/com/revature/Account.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.revature;
+
+/**
+ * @author yuri0
+ *
+ */
+public class Account {
+
+	/**
+	 * 
+	 */
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
